0838cc7b92e8b7a7743938a4ff9fd5fa1190c7cb
Gabor Gyenge <Gabor_Gyenge@epam.com>
2019-06-06 16:53:50 +0200

    Delete old tests

--- src/test/java/hu/gyengus/smarthome/alarm/AppTests.java
+++ src/test/java/hu/gyengus/smarthome/alarm/AppTests.java
@@ -1,10 +1,5 @@
 package hu.gyengus.smarthome.alarm;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -14,23 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class AppTests {
 
-    /**
-     * Check hu.gyengus.smarthome.alarm.App is exists.
-     */
     @Test
-    public void testAppClassIsExists() {
-        assertEquals("hu.gyengus.smarthome.alarm.App", App.class.getName());
-    }
-
-    /**
-     * Testing main method.
-     */
-    @Test
-    public void testMain() {
-        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        App.main(new String[0]);
-        assertEquals("Hello from alarm-service" + System.lineSeparator(), outContent.toString());
+    public void testApp() {
     }
 
 }
